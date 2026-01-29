3d1518788efd27b57c9b55d8d55655a577135dac
Jack Belmont <33494734+itsjbelmont@users.noreply.github.com>
2026-01-28 22:46:36 -0700

    Remove unused build directory

--- build/lib/navtool/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from tool import Tool
-
-if __name__ == "__main__":
-    print(f"welcome to navtool")
-
-    tool = Tool()
-    tool.add_set("mySet")
-    tool.add_key("myKey")
--- build/lib/navtool/tool.py
+++
@@ -1,9 +0,0 @@
-class Tool:
-    def __init__(self):
-        print("initializing tool")
-
-    def add_set(self, set):
-        print(f"adding new set: {set}")
-
-    def add_key(self, key):
-        print(f"Adding new key: {key}")
